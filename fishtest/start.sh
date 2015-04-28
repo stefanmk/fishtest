7fe6de7856630f2eefeca359294191268409e3c4
Stefan Meyer-Kahlen <info@shredderchess.com>
2015-04-28 10:40:30 +0200

    setup instructions completed

--- fishtest/start.sh
+++ fishtest/start.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 cd /home/fishtest/fishtest/fishtest
 
--- fishtest/start_dev.sh
+++ fishtest/start_dev.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 #nohup pserve development.ini --reload >nohup.out 2>&1 &
 pserve development.ini --reload
