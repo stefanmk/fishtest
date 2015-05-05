b7092817917af0940e42b8f97157039827ad09d5
Stefan Meyer-Kahlen <info@shredderchess.com>
2015-05-05 21:33:45 +0200

    start script and port for development mode changed

--- fishtest/start_dev.sh
+++ fishtest/start_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#nohup pserve development.ini --reload >nohup.out 2>&1 &
-pserve development.ini --reload
+nohup pserve development.ini --reload >nohup.out 2>&1 &
+#pserve development.ini --reload
